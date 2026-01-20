5b72792564fe74a20adf2c87297651df0f6aa707
Jagadishwar Reddy Mitta <mittajagadishwarreddy@gmail.com>
2026-01-20 11:19:42 +0530

    Update search.jsx

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -4,7 +4,7 @@ const search = ({searchItem, SetSearchItem}) => {
   return (
     <div className='search'>
      <div>
-        <img src='/search.svg' alt='search' />
+        <img src='./search.svg' alt='search' />
         <input 
         type='text'
         placeholder='Search your movie journey from here'
